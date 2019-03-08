d1de7a25fdf72b4a0a1cd10596c07c7059c3e899
paulosousa00 <45037062+paulosousa00@users.noreply.github.com>
2019-03-08 17:39:26 +0000

    Create exercicio04.c

---
+++ exercicio04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int bitsUm (int x)
+{
+    int r=0;
+    while (x!=0)
+    {
+        if (x%2==0)
+        {
+            x/=2;
+        }
+        
+        else
+        {
+            x/=2;
+            r++;
+        }
+    }
+    return r;
+}
+
