e04853db871cd24916bae563d08f524688ef2942
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2019-03-09 09:48:28 +0000

    Create exercicio35_inc.c

---
+++ exercicio35_inc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Incorreto
+
+int comunsOrd (int a[], int na, int b[], int nb)
+{
+    int i, j, com=0;
+    
+    for(i=0; i<na; i++)
+    {
+        for(j=0; j<nb; j++)
+        {
+            if (a[i]==b[j]) com++;
+        }
+    }
+    
+    return com;
+}
