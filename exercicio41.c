6e749a45a15fcc9eac389cf0a778d810b7665db4
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2019-03-09 10:10:45 +0000

    Create exercicio41.c

---
+++ exercicio41.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void addTo(int N, int M, int a [N][M], int b[N][M])
+{
+    int i, j;
+    
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<M; j++) a[i][j]+=b[i][j];
+    }
+}
