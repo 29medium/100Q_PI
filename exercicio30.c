9cdb41b98a9a470a1227ca4d8294cebff6583b60
Paulo Sousa <fcpf16@gmail.com>
2019-03-09 14:57:24 +0000

    Create exercicio30.c

---
+++ exercicio30.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int menosFreq (int v[], int N)
+{
+    int i, j, min=v[0], n=N, cont;
+    
+    for(i=0; i<N; i++)
+    {
+        cont=0;
+        
+        for(j=0; j<N; j++)
+        {
+            if (v[j]==v[i]) cont++;
+        }
+        
+        if (cont<n)
+        {
+            min=v[i];
+            n=cont;
+        }
+    }
+    return min;
+}
