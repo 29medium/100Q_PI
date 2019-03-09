3ae8248e8c9b7978ba893c0ad38f6e8f07780e8e
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2019-03-09 09:40:18 +0000

    Create exercicio34x.c

---
+++ exercicio34x.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Incorreto
+
+int elimRepOrd (int v[], int N)
+{
+    int i, j;
+    
+    for(i=0; i<N; i++)
+    {
+        if(v[i+1]==v[i])
+        {
+            for(j=i; j<N; j++) v[j]=v[j+1];
+            N--;
+            i--;
+        }
+    }
+    return N;
+}
