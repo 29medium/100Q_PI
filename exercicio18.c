43e2d6f8571876f963c0b5db7fbb5490bcd31089
Paulo Sousa <fcpf16@gmail.com>
2019-03-15 15:05:12 +0000

    Primeiras Perguntas

--- exercicio18.c
+++ exercicio18.c
@@ -6,14 +6,7 @@ int maiorSufixo (char s1 [], char s2 [])
 {
     int i, j, suf=0;
     
-    for(i=strlen(s1)-1, j=strlen(s2)-1; i>=0 && j>=0 ;i--, j--)
-    {
-        if (s1[i]==s2[j]) 
-        {
-            suf++;
-        }
-        
-        else break;
-    }
+    for(i=strlen(s1)-1, j=strlen(s2)-1; s1[i]==s2[j] && i>=0 ;i--, j--) suf++;
+    
     return suf;
 }
