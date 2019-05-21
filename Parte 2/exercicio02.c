1acf10515a6b46b051e0554b2944b8d4a6397e7d
Paulo Sousa <fcpf16@gmail.com>
2019-05-21 17:29:24 +0100

    Create exercicio02.c

---
+++ Parte 2/exercicio02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lligada {
+    int valor;
+    struct lligada *prox;
+} *LInt;
+
+void freeL (LInt l)
+{
+    LInt aux;
+    
+    while (l!=NULL)
+    {
+        aux=l;
+        l=l->prox;
+        free(aux);
+    }
+}
