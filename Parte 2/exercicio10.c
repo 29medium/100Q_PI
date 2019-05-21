a4ae60634c21ff30772f6327f4ca5c11be0ef184
Paulo Sousa <fcpf16@gmail.com>
2019-05-21 19:18:21 +0100

    Create exercicio10.c

---
+++ Parte 2/exercicio10.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listas.h"
+
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
+int removeAll (LInt *l, int x)
+{
+    int res=0, flag = 0;
+    LInt aux, prev;
+    
+    while (*l!=NULL && flag==0)
+    {
+        if((*l)->valor == x) 
+        {
+            *l=(*l)->prox;
+            res++;
+        }
+        
+        else flag=1;
+    }
+    
+    if (*l == NULL) return res;
+    
+    aux=*l;
+    
+    while(aux != NULL)
+    {
+        if(aux->valor == x)
+        {
+            aux=aux->prox;
+            prev->prox=aux;
+            res++;
+        }
+        else
+        {
+            prev=aux;
+            aux=aux->prox;
+        }
+    }
+    
+    return res;
+}
