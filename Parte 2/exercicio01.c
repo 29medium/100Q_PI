198ffefed8b4c3555b631f7629da4e751be5d6dc
Paulo Sousa <fcpf16@gmail.com>
2019-05-21 14:40:39 +0100

    Create exercicio01.c

---
+++ Parte 2/exercicio01.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lligada {
+    int valor;
+    struct lligada *prox;
+} *LInt;
+
+LInt newLInt (int, LInt);
+int length (LInt l);
+
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) 
+    {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
+int length (LInt l)
+{
+    int i;
+    
+    for (i=0; l!=NULL; i++)
+    {
+        l = l->prox;
+    }
+    
+    return i;
+}
