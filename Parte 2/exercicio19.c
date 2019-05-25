b210411c618c3027bcd5dca450a0db7d44c99813
Paulo Sousa <fcpf16@gmail.com>
2019-05-25 16:43:32 +0100

    Update exercicio19.c

--- Parte 2/exercicio19.c
+++ Parte 2/exercicio19.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct lligada 
-{
-    int valor;
-    struct lligada *prox;
-} *LInt;
-
-LInt newLInt (int v, LInt t)
-{
-    LInt new = (LInt) malloc (sizeof (struct lligada));
-    
-    if (new!=NULL) 
-    {
-        new->valor = v;
-        new->prox  = t;
-    }
-    return new;
-}
-
 void freeL (LInt l)
 {
     LInt aux;
