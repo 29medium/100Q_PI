5cb4f843e5fac850c14db61a814423ec5ae04b21
Paulo Sousa <fcpf16@gmail.com>
2019-05-25 18:18:51 +0100

    Create exercicio26.c

---
+++ Parte 2/exercicio26.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+LInt rotateL (LInt l)
+{
+    if (l==NULL || l->prox==NULL) return l;
+
+    LInt aux=l, newL=l;
+    l = l->prox;
+    
+    while(aux->prox!=NULL) aux=aux->prox;
+    
+    aux->prox=newL;
+    newL->prox=NULL;
+    
+    return l;
+}
