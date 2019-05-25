86bab69787a6003ee23e73f8d46beb6e839a9330
Paulo Sousa <fcpf16@gmail.com>
2019-05-25 16:40:30 +0100

    Update exercicio10.c

--- Parte 2/exercicio10.c
+++ Parte 2/exercicio10.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "listas.h"
-
-LInt newLInt (int v, LInt t){
-    LInt new = (LInt) malloc (sizeof (struct lligada));
-    
-    if (new!=NULL) {
-        new->valor = v;
-        new->prox  = t;
-    }
-    return new;
-}
 
 int removeAll (LInt *l, int x)
 {
