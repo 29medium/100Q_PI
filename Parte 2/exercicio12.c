eb713eaa22a5f76f214c7a5b2df12fbe1324e6ab
Paulo Sousa <fcpf16@gmail.com>
2019-05-25 16:41:41 +0100

    Update exercicio12.c

--- Parte 2/exercicio12.c
+++ Parte 2/exercicio12.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct lligada 
-{
-    int valor;
-    struct lligada *prox;
-} *LInt;
-
-LInt newLInt (int v, LInt t)
-{
-    LInt new = (LInt) malloc (sizeof (struct lligada));
-    
-    if (new!=NULL) 
-    {
-        new->valor = v;
-        new->prox  = t;
-    }
-    return new;
-}
-
 int posMaior (LInt l)
 {
     int i, posM=0, max= l->valor;
