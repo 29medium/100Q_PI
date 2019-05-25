88dfea2c06133caec666b88f39fc248d8a0d7c01
Paulo Sousa <fcpf16@gmail.com>
2019-05-25 16:39:13 +0100

    Update exercicio07.c

--- Parte 2/exercicio07.c
+++ Parte 2/exercicio07.c
@@ -1,47 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct lligada {
-    int valor;
-    struct lligada *prox;
-} *LInt;
-
-LInt newLInt (int v, LInt t){
-    LInt new = (LInt) malloc (sizeof (struct lligada));
-    
-    if (new!=NULL) {
-        new->valor = v;
-        new->prox  = t;
-    }
-    return new;
-}
-
 void merge (LInt *r, LInt l1, LInt l2)
 {
     *r = newLInt(0, NULL);
-    LInt curr = *r;
+    LInt aux = *r;
     
     while (l1 != NULL && l2 != NULL)
     {
         if (l1->valor <= l2->valor)
         {
-            curr->prox = l1;
+            aux->prox = l1;
             l1=l1->prox;
-            curr=curr->prox;
+            aux=aux->prox;
         }
         else
         {
-            curr->prox = l2;
+            aux->prox = l2;
             l2=l2->prox;
-            curr=curr->prox;
+            aux=aux->prox;
         }
     }
     
-    if (l1 == NULL) curr->prox = l2;
+    if (l1 == NULL) aux->prox = l2;
     
-    else if (l2 == NULL) curr->prox = l1;
+    else if (l2 == NULL) aux->prox = l1;
     
-    curr = *r;
+    aux = *r;
     *r = (*r)->prox;
-    free(curr);
+    free(aux);
 }
