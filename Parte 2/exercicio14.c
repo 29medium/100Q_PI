b841063f233274d545b50c57634335648fd01781
Paulo Sousa <fcpf16@gmail.com>
2019-05-25 16:42:27 +0100

    Update exercicio14.c

--- Parte 2/exercicio14.c
+++ Parte 2/exercicio14.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct lligada {
-    int valor;
-    struct lligada *prox;
-} *LInt;
-
-LInt newLInt (int v, LInt t)
-{
-    LInt new = (LInt) malloc (sizeof (struct lligada));
-    
-    if (new!=NULL) 
-    {
-        new->valor = v;
-        new->prox  = t;
-    }
-    return new;
-}
-
 void appendL (LInt *l, int x)
 {
     LInt aux=*l;
