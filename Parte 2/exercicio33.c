8dba7f8e1e8c9ad3caeea2282e9a3de53703ebc7
Paulo Sousa <fcpf16@gmail.com>
2019-05-26 10:56:04 +0100

    Incorreta

---
+++ Parte 2/exercicio33.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+LInt mynewLInt (int v, LInt t)
+{
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) 
+    {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
+void acLInt(int x, LInt * l)
+{
+    LInt aux = (*l), new = mynewLInt(x,NULL);
+    
+    if((*l)==NULL) 
+    {
+        *l = new;
+        return;
+    }
+    
+    while (aux->prox != NULL) aux = aux->prox;
+    
+    aux->prox = new;
+}
+
+void posorder (ABin a, LInt * l) 
+{
+    if (a!=NULL)
+    {
+        posorder (a->esq, l);
+        posorder (a->dir, l);
+        acLInt(a->valor, l);
+    }
+}
