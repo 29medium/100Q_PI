fde3a100af9178d48c459075d3c4b0f9cc30eb65
Paulo Sousa <fcpf16@gmail.com>
2019-05-28 15:17:43 +0100

    Create exercicio37.c

---
+++ Parte 2/exercicio37.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int iguaisAB (ABin a, ABin b) 
+{
+    if (a==NULL && b==NULL) return 1;
+    
+    else if (a==NULL && b!=NULL) return 0;
+    
+    else if (a!=NULL && b==NULL) return 0;
+    
+    else if (a->valor != b->valor) return 0;
+
+    else return iguaisAB(a->esq, b->esq) * iguaisAB(a->dir, b->dir);
+}
