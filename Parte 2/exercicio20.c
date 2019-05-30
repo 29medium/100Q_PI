ee72016efad8a5b1d7af6a8e387d6072f6a518ed
Paulo Sousa <fcpf16@gmail.com>
2019-05-30 20:28:39 +0100

    Correta

--- Parte 2/exercicio20.c
+++ Parte 2/exercicio20.c
@@ -3,17 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int length (LInt l)
+{
+    int i;
+    
+    for(i=0; l!=NULL; i++) l=l->prox;
+    
+    return i;
+}
+
+void freeL (LInt head)
+{
+	LInt tmp;
+    
+	while (head != NULL)
+    {
+		tmp = head;
+		head = head->prox;
+		free(tmp);
+	} 
+}
+
 int drop (int n, LInt *l)
 {
     int i;
     LInt aux=*l;
     
-    for(i=0; i<(n-1) && aux!=NULL; i++)
+    int len = length(*l);
+    
+    if(len<=n) 
+    {
+        freeL(aux);
+        *l=NULL;
+        return len;
+    }
+    
+    for(i=0; i<n ; i++)
     {
         aux=*l;
         *l=(*l)->prox;
-        free(*l);
+        free(aux);
     }
     
-    return i;
+    return n;
 }
