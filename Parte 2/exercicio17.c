2e15fbd9d625d1e99cd79c3587d571e0f86f2eb0
Paulo Sousa <fcpf16@gmail.com>
2019-05-30 18:37:56 +0100

    Correta

---
+++ Parte 2/exercicio17.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+LInt cloneRev(LInt l)
+{
+    LInt aux=NULL;
+    
+    while(l!=NULL)
+    {
+        aux = newLInt(l->valor, aux);
+        l=l->prox;
+    }
+    
+    return aux;
+}
