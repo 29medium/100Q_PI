f0d6261abf41b83c956e413f34dbf25885136ed5
Paulo Sousa <fcpf16@gmail.com>
2019-05-30 12:35:27 +0100

    Create exercicio43.c

---
+++ Parte 2/exercicio43.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ABin cloneMirror (ABin a) 
+{
+    if (a==NULL) return NULL;
+    
+    ABin mirror = (ABin) malloc(sizeof(ABin));
+    ABin newEsq = cloneMirror(a->dir);
+    ABin newDir = cloneMirror(a->esq);
+    
+    mirror->valor = a->valor;
+    mirror->esq = newEsq;
+    mirror->dir = newDir;
+    
+    return mirror;
+}
