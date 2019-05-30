78328b3f9f88645bde202bbe2051cdc362bde513
Paulo Sousa <fcpf16@gmail.com>
2019-05-30 18:38:14 +0100

    Update exercicio16.c

--- Parte 2/exercicio16.c
+++ Parte 2/exercicio16.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-LInt newLInt (int v, LInt t){
-    LInt new = (LInt) malloc (sizeof (struct lligada));
-    
-    if (new!=NULL) {
-        new->valor = v;
-        new->prox  = t;
-    }
-    return new;
-}
-
 LInt cloneL(LInt l)
 {
     LInt start;
